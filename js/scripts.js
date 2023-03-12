8a3eade3d811de13ee7e85c4c5f1fcbf231ab4c2
José Morano <j.morano@udc.es>
2023-03-12 18:24:10 +0100

    Reusable components and multi-language support.

--- js/scripts.js
+++ js/scripts.js
@@ -67,37 +67,6 @@
 
 
 	/*-------------------------------------------------------------------------------
-	  Menu
-	-------------------------------------------------------------------------------*/
-
-
-
-	$('.navbar-toggle').on('click',function(){
-		$('body').removeClass('menu-is-closed').addClass('menu-is-opened');
-	});
-
-	$('.close-menu, .click-capture, .menu-list li a').on('click', function(){
-		$('body').removeClass('menu-is-opened').addClass('menu-is-closed');
-		$('.menu-list ul').slideUp(300);
-	});
-
-	$('.menu-list li a').on('click', function(){
-		$('.menu-list li').removeClass('active');
-		$(this).closest('li').addClass('active');
-
-	});
-
-
-	$('.col-resume').on('mouseover',function(){
-		$('.section-bg.mask').addClass('hide');
-	});
-
-	$('.col-resume').on('mouseleave', function () {
-	  $('.section-bg.mask').removeClass('hide');
-	});
-
-
-	/*-------------------------------------------------------------------------------
 	  Owl Carousel
 	-------------------------------------------------------------------------------*/
 
